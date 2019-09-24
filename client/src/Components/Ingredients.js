6b39e98c50a5bf3feb2b2db208e4ee73e163d844
Andre Williams <mMmCheesecake@Andres-MBP.lan1>
2019-09-24 05:28:20 -0400

    sample

--- client/src/Components/Ingredients.js
+++ client/src/Components/Ingredients.js
@@ -1,11 +1,84 @@
 import React from 'react';
+import { meats, grains, veggies, spices} from './listOfIngredients';
 
-function Ingredients() {
+class Ingredients extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      meat: [],
+      grain: [],
+      veggie: [],
+      spice: []
+    }
+  }
+  render () {
+    let meatData = meats.map((d, i) => {
+      return (
+        <>
+          <ul>
+          <li key={i}>{d}</li>
+          </ul>
+        </>
+      )
+    })
+    let grainData = grains.map((d, i) => {
+      return (
+        <>
+          <ul>
+          <li key={i}>{d}</li>
+          </ul>
+        </>
+      )
+    })
+    let veggieData = veggies.map((d, i) => {
+      return (
+        <>
+          <ul>
+          <li key={i}>{d}</li>
+          </ul>
+        </>
+      )
+    })
+    let spiceData = spices.map((d, i) => {
+      return (
+        <>
+          <ul>
+          <li key={i}>{d}</li>
+          </ul>
+        </>
+      )
+    })
     return(
       <nav className="IngredientsComponent">
-        <h4> this is Main: Ingredients </h4>
+        <h2>Ingredients</h2>
+        <h3>What ingredients do you have? Select all that apply</h3>
+        <div className='gredients'>
+          <div className='in'>
+            <h4>Meats</h4>
+            {meatData}
+          </div>
+        </div>
+        <div className='gredients'>
+          <div className='in'>
+            <h4>Grains</h4>
+            {grainData}
+          </div>
+        </div>
+        <div className='gredients'>
+          <div className='in'>
+            <h4>Veggies</h4>
+            {veggieData}
+          </div>
+        </div>
+        <div className='gredients'>
+          <div className='in'>
+            <h4>Spices</h4>
+            {spiceData}
+          </div>
+        </div>
       </nav>
     )
+  }
 }
 
 export default Ingredients;
---
+++ client/src/Components/listOfIngredients.js
@@ -0,0 +1,4 @@
+export const meats =['chicken', 'fish', 'beef', 'pork'];
+export const grains = ['rice', 'bread', 'pasta', 'quinoa'];
+export const veggies = ['spinach', 'brocolli', 'kale', 'cauliflower'];
+export const spices = ['salt & pepper', 'cinnamon', 'kale', 'curry'];
